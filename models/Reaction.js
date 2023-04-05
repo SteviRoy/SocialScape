779c3752d0f7747e1beb211ffdfa1d88c54da661
S.P. ROY <sprithi44@gmail.com>
2023-04-04 20:49:51 -0400

    Reaction.js completed

---
+++ models/Reaction.js
@@ -0,0 +1,33 @@
+const { Schema } = require('mongoose');
+const moment = require('moment');
+
+const ReactionSchema = new Schema(
+  {
+    reactionId: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId()
+    },
+    reactionBody: {
+      type: String,
+      required: true,
+      maxlength: 280
+    },
+    username: {
+      type: String,
+      required: true
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: timestamp => moment(timestamp).format('MMM DD, YYYY [at] hh:mm A')
+    }
+  },
+  {
+    toJSON: {
+      getters: true
+    },
+    id: false
+  }
+);
+
+module.exports = ReactionSchema;
